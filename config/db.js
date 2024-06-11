8e951a74900a11b84d4fd36481311889d1f22c34
Saad-Mohamed-Saad55 <saad.mohamed55.is@gmail.com>
2024-06-12 01:40:15 +0300

    first commit

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+
+const conn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "college_platform"
+
+//   host: "bc9hbqejea54mm4jisb2-mysql.services.clever-cloud.com",
+//   user: "ueowj6fgqzxsok7q",
+//   password: "ELreF73VjN4m4pDrVnBQ",
+//   database: "bc9hbqejea54mm4jisb2",
+});
+conn.connect(function (err){
+    if(err) throw err;
+    console.log("Connected To Database :)");
+});
+
+module.exports = conn;
